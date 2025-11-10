2794894d61a12ab1710ae0349d5c5609b3753707
unknown <dexter.a.blanchard@gmail.com>
2025-11-10 00:48:44 -0700

    Finalize Tests

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ def crack_passwords(hash_path, wordlist_path, hash_type = "md5"):
     for option in target:
         if option not in return_dict:
             return_dict.update({option: "Failed"})
-
+    
+    print(return_dict)
     return return_dict
 
 def type(phrase, newline):
